24d83341398e1b7a518b9564809db082e47adb06
mjidelouss <mjid.elouss@gmail.com>
2023-11-26 14:53:49 +0100

    Configured DemandController to use ResponseEntiy

--- src/main/java/com/root/rentalheive/controllers/DemandController.java
+++ src/main/java/com/root/rentalheive/controllers/DemandController.java
@@ -2,8 +2,11 @@ package com.root.rentalheive.controllers;
 
 import com.root.rentalheive.dto.DemandDto;
 import com.root.rentalheive.entities.Demand;
+import com.root.rentalheive.entities.User;
 import com.root.rentalheive.services.DemandService;
 import com.root.rentalheive.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,12 +18,23 @@ public class DemandController {
         this.demandService = demandService;
         this.userService = userService;
     }
+
     @PostMapping("")
-    public Demand save(@RequestBody DemandDto demand){
-        return demandService.save(Demand.builder().
-                user(userService.getUserById(demand.getUserId())).
-                DemandedDate(demand.getDemandedDate()).
-                endDate(demand.getEndDate()
-                ).build());
+    public ResponseEntity<Demand> save(@RequestBody DemandDto demandDto) {
+        User user = userService.getUserById(demandDto.getUserId());
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Demand demand = Demand.builder()
+                .user(user)
+                .DemandedDate(demandDto.getDemandedDate())
+                .endDate(demandDto.getEndDate())
+                .build();
+
+        Demand savedDemand = demandService.save(demand);
+
+        return new ResponseEntity<>(savedDemand, HttpStatus.CREATED);
     }
 }
